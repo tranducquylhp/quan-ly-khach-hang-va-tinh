6ce717dfdad2b3f51996583378fe58c8d0b17a86
Trần Đức Quý <tranducquylhp@gmail.com>
2019-09-20 11:10:29 +0700

    update phan trang va tim kiem

--- src/main/java/com/codegym/cms/service/CustomerService.java
+++ src/main/java/com/codegym/cms/service/CustomerService.java
@@ -2,11 +2,13 @@ package com.codegym.cms.service;
 
 import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CustomerService {
-    Iterable<Customer> findAll();
+    Page<Customer> findAll(Pageable pageable);
 
     Customer findById(Long id);
 
@@ -15,4 +17,6 @@ public interface CustomerService {
     void remove(Long id);
 
     Iterable<Customer> findAllByProvince(Province province);
+
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
